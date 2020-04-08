c993ad492326ff12bb89d5cad137dc42b507c680
nventuri97 <n.venturi3@studenti.unipi.it>
2020-04-08 10:26:45 +0200

    Added new methods

--- Point.py
+++ Point.py
@@ -1,11 +1,13 @@
-import math
-
 """Base class Point"""
 class Point(object):
 
     def __init__(self):
         self.X=0
         self.Y=0
+    
+    def __init__(self, x, y):
+        self.X=x
+        self.Y=y
 
     def setX(self, value):
         self.X=value
@@ -18,3 +20,9 @@ class Point(object):
 
     def getY(self):
         return self.Y
+
+    def opposite(self, p):
+        W=Point()
+        W.setX(self.getX)
+        W.setY(p-self.getY)
+        return W
